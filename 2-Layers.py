0643ece2d42c060ecaca02f53201de24f45416b1
ManasSuple <manassuple163@gmail.com>
2025-03-18 23:45:07 +0530

    4th part of the Neural Network

---
+++ 2-Layers.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+inputs = [[1, 2, 3, 2.5],
+          [2., 5., -1., 2],
+          [-1.5, 2.7, 3.3, -0.8]]
+#Weights 1 and biases 1 for layer 1
+weights = [[0.2, 0.8, -0.5, 1],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2, 3, 0.5]
+
+#Weights 2 and biases 2 for layer 2
+weights2 = [[0.1, -0.14, 0.5],
+            [-0.5, 0.12, -0.33],
+            [-0.44, 0.73, -0.13]]
+
+biases2 = [-1, 2, -0.5]
+
+# Convert lists to numpy arrays
+inputs_array = np.array(inputs)
+weights_array = np.array(weights)
+biases_array = np.array(biases)
+weights2_array = np.array(weights2)
+biases2_array = np.array(biases2)
+
+# Calculate the output of the first layer
+layer1_outputs = np.dot(inputs_array, weights_array.T) + biases_array
+
+# Calculate the output of the second layer
+layer2_outputs = np.dot(layer1_outputs, weights2_array.T) + biases2_array
+
+print(layer2_outputs)
--- Coding First Neuron.py
+++ Coding First Neuron.py
@@ -1,9 +1,12 @@
 import numpy as np
 
-inputs = [1, 2, 3, 2.5]
-weights = [0.2, 0.8, -0.5, 1.0]
-bias = 2.0
-
-# output = np.dot(weights, inputs) + bias
-output = np.dot(inputs, weights) + bias
+inputs = [[1, 2, 3, 2.5], [2.0, 5.0, -1.0, 2.0], [-1.5, 2.7, 3.3, -0.8]]
+weights = [
+    [0.2, 0.8, -0.5, 1.0],
+    [0.5, -0.91, 0.26, -0.5],
+    [-0.26, -0.27, 0.17, 0.87]
+]
+bias = [2.0, 3.0, 0.5]
+# We can transpose the list in python so we have to conver weight matrix into array first so we did it by using np.array(weights).T
+output = np.dot(inputs, np.array(weights).T) + bias
 print(output)
